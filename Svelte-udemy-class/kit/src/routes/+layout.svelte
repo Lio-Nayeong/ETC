884e5e61b70af4c4b6883ef037ee50a689683050
liocer2017 <liocer2022@gmail.com>
2023-03-08 17:18:47 +0900

    Initial commit

---
+++ Svelte-udemy-class/kit/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import '../app.css'
+    
+    const NAVIGATION_MENUS = [
+    { name: "home", page: "/" },
+    { name: "intro", page: "/01-intro" },
+    { name: "base-syntax", page: "/02-base-syntax" },
+    ];
+
+</script>
+
+<div>
+    <a href="/">home</a>
+    <a href="/01-intro">intro</a>
+    <a href="/02-base-syntax">base-syntax</a>
+</div>
+
+<slot />
+
---
+++ Svelte-udemy-class/kit/src/routes/01-intro/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'Kim';
+	let age = 25;
+
+	function incrementAge() {
+		age += 1;
+	}
+</script>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
